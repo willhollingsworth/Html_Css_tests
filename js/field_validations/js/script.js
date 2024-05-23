3db893619d0de1c3360d3a789d28b6fbbfebeb85
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-05-23 15:05:22 +0930

    multiple validation checks

--- js/field_validations/js/script.js
+++ js/field_validations/js/script.js
@@ -1,9 +1,14 @@
 function validate(){
     const inputs = document.querySelectorAll('input');
     for(let field of inputs){
-        if (field.value.length == 0){
-            console.log(field.id, 'is blank');
-            field.focus();
+        const checks = [
+            field.value.length > 0,
+            field.value.length < 6,
+            !field.value.includes('!'),
+        ]
+        if (checks.some((x) => x == false)){
+            console.log(field.id + ' is failing a check');
+
             return;
         }
     }
