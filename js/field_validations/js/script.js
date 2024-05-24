9a272ed5913608da66dacea0165739e7cf5bffa5
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-05-24 18:41:01 +0930

    fixed naming of lets

--- js/field_validations/js/script.js
+++ js/field_validations/js/script.js
@@ -1,23 +1,23 @@
 function validate(){
     const INPUTS = document.querySelectorAll('input');
-    let CHECKS = {};
-    let CHECK_KEYS = [];
-    let CHECK_VALUES = [];
+    let checks = {};
+    let checkKeys = [];
+    let checkValues = [];
     let fail_index = 0;
 
     for(let field of INPUTS){
         // list all checks below, they should match true if it's a pass
-        CHECKS = {
+        checks = {
             'too small' : field.value.length > 0,
             'too large' : field.value.length < 6,
             'contains a !' : !field.value.includes('!'),
         };
-        CHECK_KEYS = Object.keys(CHECKS);
-        CHECK_VALUES = Object.values(CHECKS);
+        checkKeys = Object.keys(checks);
+        checkValues = Object.values(checks);
         // if any check fails
-        if (CHECK_VALUES.includes(false)){
-            fail_index = CHECK_VALUES.findIndex((x) => x == false)
-            console.log(field.id + ' is failing check: ' + CHECK_KEYS[fail_index] );
+        if (checkValues.includes(false)){
+            fail_index = checkValues.findIndex((x) => x == false)
+            console.log(field.id + ' is failing check: ' + checkKeys[fail_index] );
             return;
         }
     }
