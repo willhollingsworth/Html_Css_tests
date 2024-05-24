98d6fcbebe3e2e1199ffac327689639d362584a7
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-05-24 18:40:12 +0930

    validator now reports which check failed

--- js/field_validations/js/script.js
+++ js/field_validations/js/script.js
@@ -1,16 +1,23 @@
 function validate(){
-    const inputs = document.querySelectorAll('input');
-    for(let field of inputs){
+    const INPUTS = document.querySelectorAll('input');
+    let CHECKS = {};
+    let CHECK_KEYS = [];
+    let CHECK_VALUES = [];
+    let fail_index = 0;
+
+    for(let field of INPUTS){
         // list all checks below, they should match true if it's a pass
-        const checks = [
-            field.value.length > 0,
-            field.value.length < 6,
-            !field.value.includes('!'),
-        ]
+        CHECKS = {
+            'too small' : field.value.length > 0,
+            'too large' : field.value.length < 6,
+            'contains a !' : !field.value.includes('!'),
+        };
+        CHECK_KEYS = Object.keys(CHECKS);
+        CHECK_VALUES = Object.values(CHECKS);
         // if any check fails
-        if (checks.includes(false)){
-            const fail_index = checks.findIndex((x) => x == false)
-            console.log(field.id + ' is failing check:' + fail_index );
+        if (CHECK_VALUES.includes(false)){
+            fail_index = CHECK_VALUES.findIndex((x) => x == false)
+            console.log(field.id + ' is failing check: ' + CHECK_KEYS[fail_index] );
             return;
         }
     }
