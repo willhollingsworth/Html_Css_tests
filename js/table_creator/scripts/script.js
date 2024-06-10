2fcdd38b5fe8569dde244f463e0f1649c7bdc94a
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-06-10 17:53:26 +0930

    cleanup with less repeated code

--- js/table_creator/scripts/script.js
+++ js/table_creator/scripts/script.js
@@ -39,7 +39,7 @@ function buildSampleData(length){
     return out
 }
 
-function buildRow(data, type){
+function buildRowSingle(data, type){
     let tableRow = document.createElement('tr');
     for (i of data){
         let entry = document.createElement(type);
@@ -48,88 +48,39 @@ function buildRow(data, type){
     }
     return tableRow;
 }
-
-
+function buildRowMultiple(data, type){
+    let tableBodyElement = document.createElement('tbody');
+    for (i of data){
+        let row = buildRowSingle(i, 'td');
+        tableBodyElement.appendChild(row);
+    }
+    return tableBodyElement;
+}
 function buildTable(data){
-    // example code from  https://www.delftstack.com/howto/javascript/create-table-javascript/
+    /* build a html table */
 
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
+    // create a main table element
+    let tableMainElement = document.createElement('table');
 
-    let headers = buildRow(['user id','name','company'],'th');
-    thead.appendChild(headers);
-    let row1 = buildRow(['001','James','netflix'],'td');
-    tbody.appendChild(row1);
-    let row2 = buildRow(['002','Paul','facebook'],'td');
-    tbody.appendChild(row2);
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    
-    // // Adding the entire table to the body tag
-    document.body.appendChild(table);
+    //create the headers
+    let headersData = ['user id','name','company']
+    let tableHeaderElement = document.createElement('thead');
+    let headersRow = buildRowSingle(headersData,'th');
+    tableHeaderElement.appendChild(headersRow);
+    tableMainElement.appendChild(tableHeaderElement);
+
+    //create the other rows
+    sampleData =[
+        ["a", "b", "c"],
+        ["d", "e", "f"],
+    ]
+    let tableBodyElement = buildRowMultiple(sampleData)
+    tableMainElement.appendChild(tableBodyElement);
+
+    // // Adding the table to the main html body element
+    document.body.appendChild(tableMainElement);
 }
-
-function manualBuildTable(){
-    // https://www.delftstack.com/howto/javascript/create-table-javascript/
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
-
-    // Creating and adding data to first row of the table
-    let row_1 = document.createElement('tr');
-    let heading_1 = document.createElement('th');
-    heading_1.innerHTML = 'Sr. No.';
-    let heading_2 = document.createElement('th');
-    heading_2.innerHTML = 'Name';
-    let heading_3 = document.createElement('th');
-    heading_3.innerHTML = 'Company';
-
-    row_1.appendChild(heading_1);
-    row_1.appendChild(heading_2);
-    row_1.appendChild(heading_3);
-    thead.appendChild(row_1);
-
-    table.appendChild(thead);
-
-    // Creating and adding data to second row of the table
-    let row_2 = document.createElement('tr');
-    let row_2_data_1 = document.createElement('td');
-    row_2_data_1.innerHTML = '1.';
-    let row_2_data_2 = document.createElement('td');
-    row_2_data_2.innerHTML = 'James Clerk';
-    let row_2_data_3 = document.createElement('td');
-    row_2_data_3.innerHTML = 'Netflix';
-
-    row_2.appendChild(row_2_data_1);
-    row_2.appendChild(row_2_data_2);
-    row_2.appendChild(row_2_data_3);
-    tbody.appendChild(row_2);
-
-    // Creating and adding data to third row of the table
-    let row_3 = document.createElement('tr');
-    let row_3_data_1 = document.createElement('td');
-    row_3_data_1.innerHTML = '2.';
-    let row_3_data_2 = document.createElement('td');
-    row_3_data_2.innerHTML = 'Adam White';
-    let row_3_data_3 = document.createElement('td');
-    row_3_data_3.innerHTML = 'Microsoft';
-
-    row_3.appendChild(row_3_data_1);
-    row_3.appendChild(row_3_data_2);
-    row_3.appendChild(row_3_data_3);
-    tbody.appendChild(row_3);
-
-    table.appendChild(tbody);
-
-    // Adding the entire table to the body tag
-    document.body.appendChild(table);
-}
-
-
-// console.log(buildSampleData(5));
-buildTable(buildSampleData(5));
-// manualBuildTable();
+buildTable();
 
 
 // https://www.valentinog.com/blog/html-table/
