a26538273b49ee7696004337ca841b4a32038bf4
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-06-21 18:37:52 +0930

    got basic manual radio logic for align and justify

--- js/grid_creator/scripts/script.js
+++ js/grid_creator/scripts/script.js
@@ -4,10 +4,27 @@
 const radioButtons = document.querySelectorAll('input[type="radio"]');
 // add event listeners to all radio inputs
 radioButtons.forEach(elem =>
-    elem.addEventListener("change",  event => reportValue(event))
+    elem.addEventListener("change",  event => radioEvent(event))
 );
 
+// select grid element to later manipulate
+gridElement = document.querySelector("#grid");
 
-function reportValue(value) {
-    console.log(value.target.id);
+function radioEvent(event){
+    // set justify
+    let justifyFormElement = document.querySelector("#formJustify");
+    let justifyValue = justifyFormElement.elements['justify'].value;
+    changeStyle(justifyValue, "justify-items")
+
+    // set align
+    let alignFormElement = document.querySelector("#formAlign");
+    let alignValue = alignFormElement.elements['align'].value;
+    changeStyle(alignValue, "align-items")
 }
+
+
+function changeStyle(value,style) {
+    console.log("change stye " + style +" to :" + value);
+    gridElement.style[style] = value;
+}
+
