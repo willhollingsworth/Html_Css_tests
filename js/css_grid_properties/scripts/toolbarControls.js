7b8fa294b67d474086e4cc92e6546494b7738cf8
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-06-26 18:20:43 +0930

    added basic grid overlays

--- js/css_grid_properties/scripts/toolbarControls.js
+++ js/css_grid_properties/scripts/toolbarControls.js
@@ -1,6 +1,11 @@
 // TODO: Build out ui radio boxes programmatically with code and a simple object
 // TODO: build better/ more automated radio value grabbing and style setting logic
 //          - rely less on static set values except where defined in an object
+//
+// FIXME: grid lines / overlays don't work when you change justify items/ align items
+
+
+
 
 // grab all radio type inputs
 const radioButtons = document.querySelectorAll('input');
@@ -36,8 +41,30 @@ function changeGridProperties(){
     changeStyle(alignContentValue, "align-content")
 }
 
+function changeGridLinesWidth(value) {
+    selectBeforeRule().style.width = Math.floor(value) + "px"
+    console.log(value, Math.floor(value))
+}
+function changeGridLinesHeight(value) {
+    selectBeforeRule().style.height = Math.floor(value) + "px"
+}
+
+function selectBeforeRule(){
+    let cssRules = document.styleSheets[0].cssRules
+    let beforeRule
+    for (rule of cssRules) {
+        if (rule.selectorText == "#grid > div::before"){
+            beforeRule = rule
+        }
+    }
+    return beforeRule
+}
+
+
 function changeColumns(){
     let columnsState = getCheckBoxValue("columnsEnable");
+    let overlayWidth = 0;
+    let gridWidth = document.querySelector("#grid").offsetWidth;
     if (columnsState) {
         // get column count
         let columnCountValue = getSimpleValue("columnsCount");
@@ -48,6 +75,9 @@ function changeColumns(){
         // set percent format 
         if  (columnSizeValue == "Percent"){
             columnSizeValue = `${columnSizeNumberValue}%`
+            overlayWidth = gridWidth * columnSizeNumberValue / 100;
+        } else {
+            overlayWidth = gridWidth / columnCountValue
         }
         // build and use column size and count values
         let columnsCountString = `repeat(${columnCountValue}, ${columnSizeValue})`
@@ -55,11 +85,15 @@ function changeColumns(){
     } else {
         // set to default
         changeStyle("revert", "grid-template-columns")
+        overlayWidth = document.querySelector("#grid > div").offsetWidth
     }
+    changeGridLinesWidth(overlayWidth)
 }
 
 function changeRows(){
     let rowsState = getCheckBoxValue("rowsEnable");
+    let overlayHeight = 0;
+    let gridHeight = document.querySelector("#grid").offsetHeight;
     if (rowsState) {
         // get row count
         let rowCountValue = getSimpleValue("rowsCount");
@@ -70,14 +104,21 @@ function changeRows(){
         // set percent format 
         if  (rowSizeValue == "Percent"){
             rowSizeValue = `${rowSizeNumberValue}%`
+            overlayHeight = gridHeight * rowSizeNumberValue / 100;
+
+        } else {
+            overlayHeight = gridHeight / rowCountValue
         }
         // build and use row size and count values
         let rowsCountString = `repeat(${rowCountValue}, ${rowSizeValue})`
         changeStyle(rowsCountString, "grid-template-rows")
+
     } else {
         // set to default
         changeStyle("revert", "grid-template-rows")
+        overlayHeight = document.querySelector("#grid > div").offsetHeight
     }
+    changeGridLinesHeight(overlayHeight)
 }
 
 function changeStyle(value,style) {
